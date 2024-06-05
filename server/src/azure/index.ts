f43dacba2b1b6ceac1e5ec764131a7afee17c53c
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-06-06 01:30:29 +0545

    some more cleanup and project structure improved

--- server/src/azure/index.ts
+++ server/src/azure/index.ts
@@ -2,6 +2,16 @@ import path from "path";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { Request, Response } from "express";
 
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING as string;
+console.log("Connection String: ", connectionString);
+
+const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME as string;
+
+const blobServiceClient =
+  BlobServiceClient.fromConnectionString(connectionString);
+
+const containerClient = blobServiceClient.getContainerClient(containerName);
+
 export async function uploadHandler(req: Request, res: Response) {
   const { imageUrl, title, description } = req.body;
   const file = req.file?.path;
@@ -16,22 +26,21 @@ export async function uploadHandler(req: Request, res: Response) {
 }
 
 async function uploadToAzureStorage(file: string) {
-  const connectionString = process.env
-    .AZURE_STORAGE_CONNECTION_STRING as string;
-  console.log("Connection String: ", connectionString);
-  const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME as string;
-  const blobServiceClient =
-    BlobServiceClient.fromConnectionString(connectionString);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-
   const blobName = path.basename(file);
 
   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-  // Upload image to Azure Storage
   const response = await blockBlobClient.uploadFile(file);
   const imageUrl = response._response.request.url;
 
   console.log("Image uploaded successfully: ", blobName, imageUrl);
   return imageUrl;
+}
+
+export async function deleteImageFromAzureStorage(fileName: string) {
+  const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+
+  await blockBlobClient.delete();
+
+  console.log("Blob deleted successfully.");
 }

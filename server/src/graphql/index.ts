230a18dd3de398afbada111b2c022c63b790abae
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-07-07 14:44:50 +0545

    pulled from master

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -21,12 +21,7 @@ if (!fs.existsSync(uploadDir)) {
 
 export async function GraphQL() {
   const app = express();
-  app.use(
-    cors({
-      origin: "*",
-      credentials: true,
-    })
-  );
+  // app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
